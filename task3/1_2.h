c330bdfed37c9af74cc4620157eef68068154e31
Michael Dallow <mikedallow@gmail.com>
2019-11-21 11:56:01 -0800

    Fixed mistake with predictor size and updated comments

--- task3/1_2.h
+++ task3/1_2.h
@@ -1,8 +1,4 @@
 // my_predictor.h
-// This file contains a sample my_predictor class.
-// It is a simple 32,768-entry gshare with a history length of 15.
-// Note that this predictor doesn't use the whole 32 kilobytes available
-// for the CBP-2 contest; it is just an example.
 
 class my_update : public branch_update {
 public:
@@ -11,12 +7,12 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define HISTORY_LENGTH	1
+#define HISTORY_LENGTH	1 // Keep track of the last 2^HISTORY_LENGTH branches
 #define TABLE_BITS	5
 	my_update u;
 	branch_info bi;
 	unsigned int history; // SHIFT register
-	unsigned char predictors[HISTORY_LENGTH+1][1<<TABLE_BITS]; // 0000_0000 0000_0000 0000_0000 0010_0000 == 32 = 2^5
+	unsigned char predictors[1<<HISTORY_LENGTH][1<<TABLE_BITS]; // [2^HISTORY_LENGTH][2^5]
 
 	my_predictor (void) : history(0) { 
 		memset (predictors, 0, sizeof (predictors));
@@ -29,12 +25,11 @@ public:
 		// Checking to make sure it is a valid branch
 		if (b.br_flags & BR_CONDITIONAL) {
 			// b.address[6:2] & 0b0001_1111
+			// Need 5 bits of the address because we have 32 (2^5) entries in BHT
 			u.index = (b.address >> 2) & ((1<<TABLE_BITS)-1);
 
-			// 00 <= table[i] <= 11
-			// prediction is true if table[i] == 11 or 10
-			// prediction is false if table[i] == 00 or 01
-
+			// prediction is true if table[i] == 11 or 10, else false
+			// Uses history as shift register to pick a predictor
 			u.direction_prediction(predictors[history&((1<<HISTORY_LENGTH)-1)][u.index] >> 1);
 
 		} else {
@@ -55,6 +50,7 @@ public:
 			// predictor to be updated is the one corresponding to history before its updated
 			c = &predictors[history&((1<<HISTORY_LENGTH)-1)][((my_update*)u)->index];
 
+			// 2-bit predictor
 			// 00 <= c <= 11
 			if (taken) {
 				if (*c == 0) *c = 1;
